d58091d9bf45711ee041e8eb776759a0d2e2ce62
Rocío Garrido <rocioganon@gmail.com>
2021-09-09 12:17:56 +0200

    create folder structure and imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
-
+import { useState } from 'react';
+import { useQuery} from "react-query";
+// Components
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+// Styles
+import { Wrapper } from './App.styles';
 
 
 
