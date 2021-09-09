c2324cbbd98811239ef3d2823e78e41c0bf96a76
Rocío Garrido <rocioganon@gmail.com>
2021-09-09 13:03:08 +0200

    call to fake store API

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,25 @@ import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 // Styles
 import { Wrapper } from './App.styles';
+// Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
 
 
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
 
 function App() {
+  const { data, isLoading, error } = useQuery<CartItemType[]>("products", getProducts);
+  console.log(data);
+  
   return (
     <div className="App">
       Start
