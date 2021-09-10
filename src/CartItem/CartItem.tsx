9e2803fee524c3e58c1ed62e1268fd521f4f1baf
Rocío Garrido <rocioganon@gmail.com>
2021-09-10 12:32:18 +0200

    paint CartItem

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -18,7 +18,25 @@ const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
                 <p>Price: ${item.price}</p>
                 <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
             </div>
+            <div className="buttons">
+                <Button
+                    size="small"
+                    disableElevation
+                    variant="contained"
+                    onClick={() => removeFromCart(item.id)}>
+                - 
+                </Button>
+                <p>{item.amount}</p>
+                <Button
+                    size="small"
+                    disableElevation
+                    variant="contained"
+                    onClick={() => addToCart(item)}>
+                +
+                </Button>
+            </div>
         </div>
+        <img src={item.image} alt={item.title} />
     </Wrapper>
 );
 
