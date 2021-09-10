d68aecae5080906c1a922681a996da74f17d5ce6
Rocío Garrido <rocioganon@gmail.com>
2021-09-10 12:11:25 +0200

    paint CartItem

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -10,6 +10,16 @@ type Props = {
     removeFromCart: (id: number) => void;
 }
 
-const CartItem: React.FC<Props> = () => <div>Cart Item</div>;
+const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
+    <Wrapper>
+        <div>
+            <h3>{item.title}</h3>
+            <div className="information">
+                <p>Price: ${item.price}</p>
+                <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+            </div>
+        </div>
+    </Wrapper>
+);
 
 export default CartItem;
