3dceb1a17ed183bf1d90c72492943be829b4ff79
Rocío Garrido <rocioganon@gmail.com>
2021-09-10 11:16:05 +0200

    write getTotalItems function

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,8 @@ function App() {
   // console.log(data);
 
 
-  const getTotalItems = (items: CartItemType[]) => null;
+  const getTotalItems = (items: CartItemType[]) => 
+    items.reduce((acc: number, item) => acc + item.amount, 0);
 
   const handleAddToCart = (clickedItem: CartItemType) => null;
 
